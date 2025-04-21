1ef6dcf576ce768a449a82f234fa5af1376a2f09
Shatha Alalmaie <sazalalmaie@gmail.com>
2025-04-21 13:25:59 +0300

    Fix database config to use fallback SQLite

--- trip_recommendation/settings.py
+++ trip_recommendation/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'trip_recommendation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+#new 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True,
+        default='sqlite:///db.sqlite3'  # 👈 fallback to local SQLite
+    )
 }
 
 
